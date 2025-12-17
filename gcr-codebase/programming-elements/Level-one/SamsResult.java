b4c64ee7fd78ce77d026ae37c5546b41f897b2b8
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-17 13:15:00 +0530

    [Sachin] : Added SamsResult program file

---
+++ gcr-codebase/programming-elements/Level-one/SamsResult.java
@@ -0,0 +1,14 @@
+/* Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average percent mark in PCM
+I/P => NONE
+O/P => Sam’s average mark in PCM is ___ 
+*/
+
+public class SamsResult
+{
+	public static void main(String[] args)
+	{
+		int math = 94, physics = 95, chemistry = 100;
+		double average = (math + physics + chemistry) / 3;
+		System.out.println("Sam's average mark in PCM is = " + average);
+	}
+}
