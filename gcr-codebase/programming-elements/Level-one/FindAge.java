afc155b28fe1603d853e77ae669240973d1c3677
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-17 12:45:00 +0530

    [Sachin] : Added FindAge Program

---
+++ gcr-codebase/programming-elements/Level-one/FindAge.java
@@ -0,0 +1,13 @@
+/* Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
+I/P => NONE
+O/P => Harry's age in 2024 is ___ 
+*/
+
+class FindAge{
+	public static void main(String[] args){
+		int yearOfBirth = 2000;
+		int currentYear = 2024;
+		int harrysAge = currentYear - yearOfBirth;
+		System.out.println("Harry's age in 2024 is " + harrysAge);
+	}
+} 
