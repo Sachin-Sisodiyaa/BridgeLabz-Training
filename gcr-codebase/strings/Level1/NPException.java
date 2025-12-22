25645ce2ce5b173d5d8cb90ea9ac1355442fccfb
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-23 00:37:42 +0530

    [Sachin] : Added NullPointerException Program File

---
+++ gcr-codebase/strings/Level1/NPException.java
@@ -0,0 +1,32 @@
+/*Write a program to demonstrate NullPointerException. 
+Hint => 
+Write a Method to generate the Exception. Here define the variable text and initialize it to null. Then call one of the String Method to generate the exception
+Write the Method to demonstrate NullPointerException. Here define the variable text and initialize it to null. Then write try catch block for handling the Exception while accessing one of the String method
+From the main Firstly call the method to generate the Exception then refactor the code to call the method to handle the RuntimeException
+*/
+
+
+
+public class NPException{
+	
+	//gerenating exception
+	public static void generateException(){
+		String text = null;
+		System.out.println(text.charAt(text.length()));
+	}
+
+	//handle exception
+	public static void RuntimeException(){
+		try{
+			generateException();
+		} 
+		catch(NullPointerException e){
+			System.out.print("NullPointerException handled successfully");
+		}
+	}
+
+	//Main method
+	public static void main(String[] args){
+		RuntimeException();
+	}
+}
