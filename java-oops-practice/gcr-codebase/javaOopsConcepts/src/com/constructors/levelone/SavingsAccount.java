bcf4a14d474d8b7ca55a1f39d5202fda364d4443
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-27 23:21:58 +0530

    [Sachin] : Added SavingsAccount.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/constructors/levelone/SavingsAccount.java
@@ -0,0 +1,15 @@
+package com.constructors.levelone;
+
+public class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(String accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);   
+        System.out.println("Account Holder : " + accountHolder);   
+        System.out.println("Balance        : ₹" + getBalance());   
+    }
+}
+
