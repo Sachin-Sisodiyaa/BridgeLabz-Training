f6e5f45f584ef3ee4303fffcf1acb02ad0d0dadd
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-31 23:05:06 +0530

    [Sachin] : Added Employee.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Employee.java
@@ -0,0 +1,17 @@
+package com.objectmodeling;
+
+public class Employee {
+
+    private int empId;
+    private String empName;
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public void displayEmployee() {
+        System.out.println("Employee ID   : " + empId);
+        System.out.println("Employee Name : " + empName);
+    }
+}
