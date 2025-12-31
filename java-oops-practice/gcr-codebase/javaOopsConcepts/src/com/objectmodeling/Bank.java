ba6afb31ff6d3dee4d5fa3c046ab116a54bbc44d
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2025-12-31 22:56:18 +0530

    [Sachin] : Added Bank.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Bank.java
@@ -0,0 +1,22 @@
+package com.objectmodeling;
+
+public class Bank {
+
+    private String bankName;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    // Method to open an account for a customer
+    public void openAccount(Customer customer, double initialBalance) {
+        customer.setBank(this);
+        customer.setBalance(initialBalance);
+        System.out.println("Account opened for " + customer.getName()
+                + " in " + bankName);
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+}
