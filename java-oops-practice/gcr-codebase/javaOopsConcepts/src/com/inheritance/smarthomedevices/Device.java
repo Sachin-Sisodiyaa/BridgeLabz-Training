946681e27cacf1a9b1287a39d6ca811d5f23ca85
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-02 11:12:57 +0530

    [Sachin] : Added Device.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/inheritance/smarthomedevices/Device.java
@@ -0,0 +1,20 @@
+package com.inheritance.smarthomedevices;
+
+public class Device {
+
+    protected String deviceId;
+    protected boolean status; // true = ON, false = OFF
+
+    // Constructor
+    public Device(String deviceId, boolean status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    // Method to display device status
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + (status ? "ON" : "OFF"));
+    }
+}
+
