90d2c3cb5e70e864c025f5023feff87e87c7997f
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-02 11:09:18 +0530

    [Sachin] : Added Teacher.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/inheritance/schoolsystemwithdifferentroles/Teacher.java
@@ -0,0 +1,18 @@
+package com.inheritance.schoolsystemwithdifferentroles;
+
+public class Teacher extends Person {
+
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public void displayRole() {
+        System.out.println("Role: Teacher");
+        displayBasicInfo();
+        System.out.println("Subject: " + subject);
+    }
+}
+
