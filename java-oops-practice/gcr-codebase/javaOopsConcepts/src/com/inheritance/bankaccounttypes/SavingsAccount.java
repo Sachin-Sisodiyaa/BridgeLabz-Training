f92eba333732815fbc9d345fa8b0c6de97319810
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-02 10:45:25 +0530

    [Sachin] : Added SavingsAccount.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/inheritance/bankaccounttypes/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.inheritance.bankaccounttypes;
+
+public class SavingsAccount extends BankAccount {
+
+    private double interestRate;
+
+    public SavingsAccount(int accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type: Savings Account");
+        displayDetails();
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+}
+
