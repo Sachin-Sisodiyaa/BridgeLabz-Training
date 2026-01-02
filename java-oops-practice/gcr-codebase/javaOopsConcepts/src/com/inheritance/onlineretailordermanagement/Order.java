c65f68bfc496fe553ab25838065d133c17cb2ba7
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-02 02:27:19 -0300

    [Sachin] : Added Order.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/inheritance/onlineretailordermanagement/Order.java
@@ -0,0 +1,17 @@
+package com.inheritance.onlineretailordermanagement;
+
+public class Order {
+
+    protected int orderId;
+    protected String orderDate;
+
+    public Order(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return "Order Placed";
+    }
+}
+
