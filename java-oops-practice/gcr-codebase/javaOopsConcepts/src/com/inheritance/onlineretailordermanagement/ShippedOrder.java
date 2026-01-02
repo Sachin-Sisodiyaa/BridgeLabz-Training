b0c7474b4b44b0523b3f5b3f17ab94a28b75e69a
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-02 02:28:06 -0300

    [Sachin] : Added ShippedOrder.java program file

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/inheritance/onlineretailordermanagement/ShippedOrder.java
@@ -0,0 +1,17 @@
+package com.inheritance.onlineretailordermanagement;
+
+public class ShippedOrder extends Order {
+
+    protected String trackingNumber;
+
+    public ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Shipped";
+    }
+}
+
