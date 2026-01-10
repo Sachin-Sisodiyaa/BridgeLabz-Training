1fc66999f96e082309f2a17479b5ba26d20d8c48
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-10 11:14:25 +0530

    [Sachin] : Added SearchIn2DMatrix.java program file

---
+++ java-dsa-practice/gcr-codebase/javaDSAPractice/src/javaDSAPractice/com/algorithms/searchingalgorithms/binarysearch/SearchIn2DMatrix.java
@@ -0,0 +1,57 @@
+package javaDSAPractice.com.algorithms.searchingalgorithms.binarysearch;
+
+import java.util.Scanner;
+
+public class SearchIn2DMatrix {
+
+    static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        boolean found = searchMatrix(matrix, target);
+        System.out.println("Target found: " + found);
+
+        sc.close();
+    }
+}
