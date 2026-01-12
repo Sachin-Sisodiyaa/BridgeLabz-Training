65fedd1dcf282d906b36bcc4e1460b02c58ffc59
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-12 14:57:46 +0530

    [Sachin] : Added StringConcatenation.java program file

---
+++ java-dsa-practice/gcr-codebase/javaDSAPractice/src/javaDSAPractice/com/analysis/stringconcatenation/StringConcatenation.java
@@ -0,0 +1,12 @@
+package javaDSAPractice.com.analysis.stringconcatenation;
+
+public class StringConcatenation {
+
+    public void concatenate(int n) {
+        String s = "";
+
+        for (int i = 0; i < n; i++) {
+            s = s + "hello";
+        }
+    }
+}
