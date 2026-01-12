a735362fe1bc9aaff700d8fdab76603a1594e0ad
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-12 14:29:51 +0530

    [Sachin] : Added BubbleSort program files

---
+++ java-dsa-practice/gcr-codebase/javaDSAPractice/src/javaDSAPractice/com/algorithms/sortingalgorithms/bubblesort/BubbleSortMain.java
@@ -0,0 +1,47 @@
+/*
+ * 
+ * 1. Bubble Sort - Sort Student Marks
+Problem Statement:
+A school maintains student marks in an array. Implement Bubble Sort to sort the student marks in ascending order.
+Hint:
+Traverse through the array multiple times.
+Compare adjacent elements and swap if needed.
+Repeat the process until no swaps are required.
+
+ * 
+ */
+
+
+package javaDSAPractice.com.algorithms.sortingalgorithms.bubblesort;
+
+import java.util.Scanner;
+
+// Main class with user input
+public class BubbleSortMain {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+
+        System.out.println("Enter student marks:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        StudentMarks sm = new StudentMarks(marks);
+
+        System.out.println("\nMarks before sorting:");
+        sm.display();
+
+        // Sorting
+        BubbleSort.sort(sm.getMarks());
+
+        System.out.println("\nMarks after Bubble Sort:");
+        sm.display();
+    }
+}
