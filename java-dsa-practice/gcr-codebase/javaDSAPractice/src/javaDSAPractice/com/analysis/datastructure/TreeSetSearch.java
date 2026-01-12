616c27f2fb88c84982967176f4b3a6f4ef950feb
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-12 14:42:19 +0530

    [Sachin] : Added TreeSetSearch.java program file

---
+++ java-dsa-practice/gcr-codebase/javaDSAPractice/src/javaDSAPractice/com/analysis/datastructure/TreeSetSearch.java
@@ -0,0 +1,11 @@
+package javaDSAPractice.com.analysis.datastructure;
+
+import java.util.TreeSet;
+
+public class TreeSetSearch {
+
+    public boolean search(TreeSet<Integer> set, int target) {
+        return set.contains(target);
+    }
+}
+
