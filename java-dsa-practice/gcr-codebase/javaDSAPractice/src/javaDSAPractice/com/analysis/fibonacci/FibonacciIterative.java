30560f7e1156f49ec172b8ef95b9e1fdb445d182
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-12 14:45:26 +0530

    [Sachin] : Added FibonacciIterative.java program file

---
+++ java-dsa-practice/gcr-codebase/javaDSAPractice/src/javaDSAPractice/com/analysis/fibonacci/FibonacciIterative.java
@@ -0,0 +1,18 @@
+package javaDSAPractice.com.analysis.fibonacci;
+
+public class FibonacciIterative {
+
+    public int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        int a = 0, b = 1, sum = 0;
+
+        for (int i = 2; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
