fb8952c787f5f449056b11a8877c94a2b07e815a
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-27 14:48:52 +0530

    [Sachin] : Added ExamResultUploader program files

---
+++ java-collections-practice/scenario-based/scenarioBased/src/com/daytwo/examresultuploader/InvalidMarkFormatException.java
@@ -0,0 +1,7 @@
+package com.daytwo.examresultuploader;
+
+public class InvalidMarkFormatException extends Exception {
+    public InvalidMarkFormatException(String message) {
+        super(message);
+    }
+}
