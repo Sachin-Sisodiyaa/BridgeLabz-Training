d284ffd2810f41f3af0965958663e7fea2654bf8
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-01-27 14:33:25 +0530

    [Sachin] : Added GenerateaJsonRepresentation program files

---
+++ java-collections-practice/gcr-codebase/javaCollectionsFramework/src/com/reflections/generateaJSONrepresentation/JsonPerson.java
@@ -0,0 +1,11 @@
+package com.reflections.generateaJSONrepresentation;
+
+public class JsonPerson {
+    public String name;
+    public int age;
+
+    public JsonPerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
