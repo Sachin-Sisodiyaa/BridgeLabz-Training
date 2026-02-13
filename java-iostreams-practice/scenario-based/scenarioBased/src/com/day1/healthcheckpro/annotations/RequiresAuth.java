a13fc92a9e7046629b937e98efc8451f53fc3b10
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-02-13 15:24:44 +0530

    [Sachin] : Added HealthCheckPro program files

---
+++ java-iostreams-practice/scenario-based/scenarioBased/src/com/day1/healthcheckpro/annotations/RequiresAuth.java
@@ -0,0 +1,10 @@
+package com.day1.healthcheckpro.annotations;
+
+import com.day1.healthcheckpro.*;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RequiresAuth {
+}
