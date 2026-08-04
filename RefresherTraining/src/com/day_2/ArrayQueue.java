f7c540e0eba8aebe216eb78155ef8e88dd22e74d
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-08-04 09:52:22 +0530

    [Sachin] : Added Day-2 Coding Problems code

---
+++ RefresherTraining/src/com/day_2/ArrayQueue.java
@@ -0,0 +1,67 @@
+package com.day_2;
+
+public class ArrayQueue {
+	int[] arr = new int [5];
+	int front = 0;
+	int rare = -1;
+	int size = arr.length;
+	
+	public void enqueue(int data) {
+		if(rare == size - 1) {
+			System.out.println("Queue is full");
+			return;
+		}
+		arr[++rare] = data;
+	}
+	
+	public int dequeue() {
+		if(front > rare) {
+			System.out.println("Queue is empty");
+			return -1;
+		}
+		int removed = arr[front];
+		front++;
+		return removed;
+	}
+	
+	public int peek() {
+		if(front > rare) {
+			System.out.println("Queue is Empty");
+			return -1;
+		}
+		return arr[front];
+	}
+	
+	public void print() {
+		if(front > rare) {
+			System.out.println("Queue is Empty");
+		}
+		for(int i=front; i<=rare; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+	
+	
+	
+	public static void main(String args[]) {
+		ArrayQueue aq = new ArrayQueue();
+		
+		aq.enqueue(1);
+		aq.enqueue(2);
+		aq.enqueue(3);
+		aq.enqueue(4);
+		aq.enqueue(5);
+		
+		
+		aq.print();
+		
+		System.out.println("Removed element: " + aq.dequeue());
+		
+		System.out.println("Removed element: " + aq.dequeue());
+		
+		System.out.println("Peek element: " + aq.peek());
+		
+		aq.print();
+	}
+}
