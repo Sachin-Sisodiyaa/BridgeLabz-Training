e3a6c1bc63d179df5121a132f2e1a0ec4c7b6178
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-08-05 17:52:47 +0530

    [Sachin] : Added Day-4 Coding Problem code

---
+++ RefresherTraining/src/com/day_4/InsertionSort1.java
@@ -0,0 +1,66 @@
+package com.day_4;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort1' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void printArray(List<Integer> arr) {
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+    // Write your code here
+    for(int i=1; i<n; i++){
+        int curr = arr.get(i);
+        int prev = i - 1;
+        
+        while(prev >= 0 && arr.get(prev) > curr){
+            arr.set(prev + 1, arr.get(prev));
+            printArray(arr);
+            prev--;
+        }
+        arr.set(prev + 1, curr);
+        
+    }
+    printArray(arr);
+
+
+    }
+
+}
+
+public class InsertionSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.insertionSort1(n, arr);
+
+        bufferedReader.close();
+    }
+}
---
+++ RefresherTraining/src/com/day_4/RunningTimeOfAlgorithms.java
@@ -0,0 +1,63 @@
+package com.day_4;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<Integer> arr) {
+        int count = 0;
+    // Write your code here
+    for(int i=1; i<arr.size(); i++){
+        int curr = arr.get(i);
+        int prev = i - 1;
+        
+        
+        while(prev >= 0 && arr.get(prev) > curr){
+            arr.set(prev + 1, arr.get(prev));
+            count++;
+            prev--;
+        }
+        arr.set(prev + 1, curr);
+        
+    }
+    return count;
+    }
+
+}
+
+public class RunningTimeOfAlgorithms {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
