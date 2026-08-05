34c50bfcff5f44b4f087fcbb2c95191c625f2848
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-08-05 18:13:39 +0530

    [sachin] : Added Day-4 Coding Problem code

---
+++ RefresherTraining/src/com/day_4/SelectionSort.java
@@ -0,0 +1,31 @@
+package com.day_4;
+
+public class SelectionSort {
+	
+	public static void sort(int arr[]) {
+		for(int i=0; i<arr.length-1; i++) {
+			int minPos = i;
+			for(int j=0; j<arr.length; j++) {
+				if(arr[minPos] > arr[j]) {
+					minPos = j;
+				}
+			}
+			int temp = arr[minPos];
+			arr[minPos] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {4,2,1,7,3};
+		
+		sort(arr);
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
