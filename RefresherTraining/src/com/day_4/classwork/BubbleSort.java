6605e7ba99c20e78de725dc2c67a809c6110719e
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-08-12 15:42:45 +0530

    [Sachin] : Added Day-4 Coding Problems code

---
+++ RefresherTraining/src/com/day_4/classwork/BubbleSort.java
@@ -0,0 +1,50 @@
+package com.day_4.classwork;
+import java.util.*;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
