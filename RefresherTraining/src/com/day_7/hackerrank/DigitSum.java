4254946a7a86dce37c9fae0796b6d4bcbbb49fcf
Sachin-Sisodiyaa <sachin.sisodiya148@gmail.com>
2026-08-12 15:34:46 +0530

    [Sachin] : Added Day-7 Coding Problems code

--- RefresherTraining/src/com/day_7/hackerrank/DigitSum.java
+++ RefresherTraining/src/com/day_7/hackerrank/DigitSum.java
@@ -1,34 +1,52 @@
 package com.day_7.hackerrank;
 
 public class DigitSum {
+//	public static int superDigit(String n, int k) {
+//
+//        long sum = 0;
+//
+//        for (char c : n.toCharArray()) {
+//            sum += c - '0';
+//        }
+//
+//        sum = sum * k;
+//
+//        return findSuperDigit(sum);
+//    }
+//
+//    public static int findSuperDigit(long num) {
+//
+//        if (num < 10) {
+//            return (int) num;
+//        }
+//
+//        long sum = 0;
+//
+//        while (num > 0) {
+//            sum += num % 10;
+//            num = num / 10;
+//        }
+//
+//        return findSuperDigit(sum);
+//    }
+	
+	
 	public static int superDigit(String n, int k) {
+	    // Write your code here
+	        long sum = 0;
+	        
+	        for(char c : n.toCharArray()){
+	            sum += c - '0';
+	        }
+	        sum *= k;
+	        
+	        if(sum < 10){
+	            return (int) sum;
+	        }
+	        
+	        return superDigit(String.valueOf(sum), 1);
 
-        long sum = 0;
-
-        for (char c : n.toCharArray()) {
-            sum += c - '0';
-        }
-
-        sum = sum * k;
-
-        return findSuperDigit(sum);
-    }
-
-    public static int findSuperDigit(long num) {
-
-        if (num < 10) {
-            return (int) num;
-        }
-
-        long sum = 0;
-
-        while (num > 0) {
-            sum += num % 10;
-            num = num / 10;
-        }
-
-        return findSuperDigit(sum);
-    }
+	    }
 	
     public static void main(String[] args) {
 
